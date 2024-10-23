649df56af66e100961a6bd23de6378563574217d
Bernt Popp <bernt.popp@gmail.com>
2024-10-23 22:03:53 +0200

    Refactor: Remove existing config.js before generating new config.js

--- generate_config.sh
+++ generate_config.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# first remove the existing config.js
+rm -f /usr/share/nginx/html/ressources/js/config.js
+
 # Replace the placeholder in config.template.js with the actual API URL
 envsubst < /usr/share/nginx/html/ressources/js/config.template.js > /usr/share/nginx/html/ressources/js/config.js
 
