8df99709f104d45e1cbe511d77109a9deed40dc8
Bernt Popp <bernt.popp@gmail.com>
2024-12-03 11:27:06 +0100

    chore(version): update frontend version to 0.17.0 closes #40

--- ressources/js/version.js
+++ ressources/js/version.js
@@ -3,7 +3,7 @@
 import { logMessage } from './log.js'; // Import the logMessage function
 
 // Frontend Version
-const frontendVersion = '0.16.1'; // Update this as needed
+const frontendVersion = '0.17.0'; // Update this as needed
 
 // API Endpoint for Versions
 const versionEndpoint = `${window.CONFIG.API_URL}/version/`;
